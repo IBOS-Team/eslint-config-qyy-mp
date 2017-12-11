356f3142805bd778d24ac20af64175c1d83b708f
inakiz <zcs92517@gmail.com>
2017-12-11 18:17:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    // "standard",
+    require.resolve('./rules')
+  ],
+  // "plugins": [
+  //   "standard",
+  //   "promise"
+  // ],
+  // "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      // http://es6.ruanyifeng.com/#docs/object#对象的扩展运算符 
+      "experimentalObjectRestSpread": true
+    }
+  },
+  "globals": {
+    "wx": true,
+    "App": true,
+    "Page": true,
+    "getApp": true,
+    "getCurrentPages": true
+  },
+  "rules": {}
+};
